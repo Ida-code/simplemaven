5afe455d720689b9b094473604346db1b3ec3f98
Ida Sharon <idasharon.r@gmail.com>
2026-04-02 00:06:36 +0530

    Implement basic arithmetic operations in App class
    Added basic arithmetic operations: add, subtract, multiply, and divide.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,1 +1,34 @@
+package com.example
 
+public class App{
+
+public int add(int a, int b ){
+  return a+b;
+
+}
+
+public int sub(int a, int b ){
+  return a-b;
+
+}
+
+
+public int multiply(int a, int b ){
+  return a*b;
+
+}
+
+public int divide(int a, int b ){
+  return a/b;
+
+}
+
+public static void main(String args[]){
+  App cal=new App();
+  int result=calc.add(10,5);
+  System.out.println("Result of addition is"+result)
+
+    }
+}
+  
+    
