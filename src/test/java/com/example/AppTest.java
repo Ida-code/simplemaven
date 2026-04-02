7a5b208aff37b492d6f541c099d33befab43f1e2
Ida Sharon <idasharon.r@gmail.com>
2026-04-02 07:34:14 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,1 +1,32 @@
+package com.example;
+
+import org.junit.test;
+import static org.junit.assert.assertEquals;
+
+
+public class AppTest{
+
+  App calc=new App();
+
+  @Test
+  public void testAddition(){
+    assertEquals(15,calc.add(10,5));
+
+  }
+  @Test
+  public void testSubtraction(){
+    assertEquals(5,calc.subtract(10,5))
+      }
+
+
+  @Test
+  public void testMulti(){
+    assertEquals(50,calc.multiply(10,5))
+      }
+
+  @Test
+  public void testDiv(){
+    assertEquals(2,calc.divide(10,5))
+      }
+
 
